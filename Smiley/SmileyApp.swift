23a56ed3d006c9c89038ba7631d309b0fdfceac4
Ulrik Lind <ulriklind@gmail.com>
2023-08-03 14:34:01 +0200

    Initial Commit

---
+++ Smiley/SmileyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmileyApp.swift
+//  Smiley
+//
+//  Created by U. Lind Mortensen on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SmileyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
